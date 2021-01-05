73f2b88b71b00bd3153a2b0c066c7e54f1594836
Kgtoledoc <kgtoledoc19@gmail.com>
2021-01-05 07:58:18 -0500

    Added some puglins and fix errors

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,42 +1,62 @@
 import { Component } from "@angular/core";
+import { LocalNotification, Plugins } from "@capacitor/core";
 import { ModalController } from "@ionic/angular";
 import { ExploreContainerComponent } from "../explore-container/explore-container.component";
 import { NewTasksPage } from "../pages/new-tasks/new-tasks.page";
-
+const { LocalNotifications } = Plugins;
 @Component({
   selector: "app-tab2",
   templateUrl: "tab2.page.html",
   styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
-  public tasks: Array<any> = [];
+  public tasks: Array<any>;
 
   constructor(public modalController: ModalController) {}
 
-  onInit() {
-    this.tasks = JSON.parse(localStorage.getItem("tasks"));
+  async ngOnInit() {
+    this.tasks = JSON.parse(localStorage.getItem("tasks"))
+      ? JSON.parse(localStorage.getItem("tasks"))
+      : [];
+
+    console.log(this.tasks);
+
+    await LocalNotifications.requestPermissions();
+  }
+
+  async scheduleBasic() {
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: "Local Notification 1",
+          body: "First Basic Local Notification",
+          id: 1,
+          extra: {
+            data: "Pass data to your handler",
+          },
+          iconColor: "#0000FF",
+          schedule: {
+            at: new Date(Date.now() + 1000 * 3),
+          },
+        },
+      ],
+    });
   }
 
   async addTasks() {
-    const modal = await this.modalController.create({
+    this.scheduleBasic();
+    /*  const modal = await this.modalController.create({
       component: NewTasksPage,
     });
     modal.onDidDismiss().then((d) => {
-      console.log(d.data["dismissed"]);
+      console.log(d);
       if (d.data["dismissed"] != true) {
-        console.log("no dismiss");
         this.tasks.push(d.data);
         localStorage.setItem("tasks", JSON.stringify(this.tasks));
-      } else {
-        console.log("Dismiss");
       }
-
-      console.log("False");
-
       console.log(this.tasks);
-      console.log("This");
     });
 
-    return await modal.present();
+    return await modal.present(); */
   }
 }
