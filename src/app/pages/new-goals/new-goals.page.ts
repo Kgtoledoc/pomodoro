18b819dd5b9645efceeeb8c295ad1139f89f67b9
Kevin Giorgio Toledo De la cruz <ktoledo@bancodebogota.com.co>
2022-04-16 21:33:55 -0500

    Add task page functionality

--- src/app/pages/new-goals/new-goals.page.ts
+++ src/app/pages/new-goals/new-goals.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 
+import { v4 as uuidv4 } from "uuid";
+
 @Component({
   selector: "app-new-goals",
   templateUrl: "./new-goals.page.html",
   styleUrls: ["./new-goals.page.scss"],
 })
 export class NewGoalsPage implements OnInit {
+
+  activityStored: Array<any>;
+  activitySelected: String;
+  taskTitle: String;
+
   constructor(private modalController: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activityStored = JSON.parse(localStorage.getItem("scheduledTasks"))
+    ? JSON.parse(localStorage.getItem("scheduledTasks"))
+    : [];
+  }
 
   dismiss() {
     this.modalController.dismiss({
@@ -17,7 +28,24 @@ export class NewGoalsPage implements OnInit {
     });
   }
 
-  createNewGoal() {
-    console.log("create new goals");
+  createNewTask() {
+    let arrayStored = this.activityStored;
+    let task = {
+      title: this.taskTitle,
+      id: uuidv4(),
+      isChecked: false,
+    };
+    arrayStored.forEach(e => {
+      if(e.id == this.activitySelected){
+        e.subtaskArray.push(task)
+        return
+      }
+    })
+    console.log(arrayStored);
+    localStorage.setItem(
+      "scheduledTasks",
+      JSON.stringify(arrayStored)
+    );
+    this.modalController.dismiss(arrayStored);
   }
 }
